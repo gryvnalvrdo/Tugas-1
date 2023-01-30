ade6fffd5a9be5a59585922b94dead5b92cc8331
gryvnalvrdo <122882404+gryvnalvrdo@users.noreply.github.com>
2023-01-30 19:49:43 +0800

    Add files via upload

---
+++ JavaApplication19.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication19;
+
+/**
+ *
+ * @author alver
+ */
+public class JavaApplication19
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        int x;
+        System.out.print("Masukkan angka bulan = ");
+        x=scan.nextInt();
+        // TODO code application logic here
+    }
+    
+}
